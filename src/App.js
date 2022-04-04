4e79b18c3bfe356a0a5f1ae6cc8b2accc1446aa1
SangMin <sangmin4208@gmail.com>
2022-04-04 20:36:30 +0900

    You're doing great! 🌮

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
-const Father = styled.div`
-  display: flex;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+  50%{
+    transform: rotate(360deg);
+    border-radius: 50%;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
 `
 
-const Input = styled.input.attrs({ require: true, minLength: 10 })`
-  background-color: toamto;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Emoji = styled.span`
+  font-size: 40px;
+`
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 2s linear infinite;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* span {
+    font-size: 40px;
+    transition: 1s;
+    &:hover {
+      transform: scale(1.5);
+    }
+    &:active {
+      opacity: 0;
+    }
+  } */
+
+  ${Emoji} {
+    transition: 1s;
+    &:hover {
+      transform: scale(1.5);
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper as="main">
+      <Box>
+        {/* <span>😇</span> */}
+        <Emoji>😎</Emoji>
+      </Box>
+    </Wrapper>
   )
 }
 
