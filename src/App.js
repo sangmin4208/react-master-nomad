161a01722ec6c30a6a253bbcffb145961b732e34
SangMin <sangmin4208@gmail.com>
2022-04-04 17:44:11 +0900

    You're doing great! 🐶

--- src/App.js
+++ src/App.js
@@ -3,27 +3,24 @@ const Father = styled.div`
   display: flex;
 `
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
   width: 100px;
   height: 100px;
-`
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+  background-color: ${(props) => props.bgColor || 'pink'};
 `
 
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50%;
 `
+
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box />
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      <Circle />
+      <Circle bgColor="purple" />
     </Father>
   )
 }
