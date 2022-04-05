88090e57c17ab5664ce52285b0f9c64776d359bb
SangMin <sangmin4208@gmail.com>
2022-04-05 22:17:21 +0900

    You're doing great! 🤩

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import Circle from "./Circle"
+import React, { FormEventHandler, useState } from "react"
 
 const App = () => {
+  const [value, setValue] = useState("")
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { currentTarget: { value } } = event;
+    setValue(value)
+  }
+  const onSubmit: FormEventHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const target = event.target as typeof event.target & {
+      value: string
+    }
+    const name = target.value
+    console.log(name)
+  }
+
   return (
-    <>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" />
-    </>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Enter your name"
+          value={value}
+          onChange={onChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import App from './App'
 
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<App />);
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
