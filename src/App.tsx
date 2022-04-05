68057039aa5deb5b0c38dc93eb92f01245e355a1
SangMin <sangmin4208@gmail.com>
2022-04-05 19:40:41 +0900

    You're doing great!

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle"
+
+const App = () => {
+  return (
+    <>
+      <Circle bgColor="teal" borderColor="yellow" />
+      <Circle bgColor="tomato" />
+    </>
+  )
+}
+
+export default App
